7ee1d49bc8632a53c5879f53e54ed150cd4c3f6e
Noah Salaman <nsalaman@iu.edu>
2019-09-30 14:57:49 -0400

    Second Commit?

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to load this stuff.
+          Edit <code>src/App.js</code> to load this stuff.
         </p>
         <a
           className="App-link"
