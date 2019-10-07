bc64a4433c56e17acf67ceadf58972f08e7716e5
Jacob LaMere <jacoblamere@gmail.com>
2019-10-07 15:27:21 -0400

    passed data array through props into wrapper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './Data';
 import './App.css';
-import Box from "./Components/Box";
 import Wrapper from "./Components/Wrapper";
 
 function App() {
   return (
     <div className="App">
-      <Wrapper/>
+      <Wrapper funkos={data}/>
     </div>
   );
 }
