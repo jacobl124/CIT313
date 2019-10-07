20a789a5225aff7d76a499a01e6058447ba069cc
nsalaman <nsalaman@tesla.cs.iupui.edu>
2019-10-07 15:50:03 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import Box from "./Components/Box";
 import Wrapper from "./Components/Wrapper";
+import Nav from './components/Nav';
 
 function App() {
+  const [/*selectedType,*/ updateSelectedType] = React.useState('All');
+
+  function changeType(type){
+      updateSelectedType(type);
+
+  }
+  
   return (
     <div className="App">
+      <Nav fnc={changeType}/>
       <Wrapper/>
     </div>
   );
